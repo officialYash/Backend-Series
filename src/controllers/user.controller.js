6c8c54468c243f20ffeca3396767e570f5fe18a0
officialYash <38553961+officialYash@users.noreply.github.com>
2024-06-24 11:36:08 +0530

    create user routes

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,9 @@
+import {aysncHandler} from '../utils/asyncHandler.js'
+
+const registerUser = aysncHandler(async (req,res) =>{
+ res.status(200).json({
+    message:"okay"
+})
+})
+
+export{registerUser}
